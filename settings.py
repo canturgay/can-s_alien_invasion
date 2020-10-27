0bd0194a2b549a358c111eaa11c99ac15318c7f7
can.turgay <can.turgay@code.berlin>
2020-10-27 11:35:39 +0100

    alien fleet

--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ class Settings:
         self.bg_colour = (0, 0, 0)
 
         #Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 3
 
         #Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_colour = (76, 76, 255)
