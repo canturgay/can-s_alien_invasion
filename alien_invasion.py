e4aeb143430b64420ba59a911e676d252facee50
can.turgay <can.turgay@code.berlin>
2020-10-27 12:15:45 +0100

    alien fleet moves horizontal and vertical

--- alien_invasion.py
+++ alien_invasion.py
@@ -60,6 +60,20 @@ class AlienInvasion:
         alien.rect.y =alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop teh entire fleet and change the fleet's direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+            
+
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -128,7 +142,9 @@ class AlienInvasion:
         self.aliens.draw(self.screen)
 
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet"""
+        """Check if the fleet is at an edge
+        then update the positions of all aliens in the fleet"""
+        self._check_fleet_edges()
         self.aliens.update()
 
 
